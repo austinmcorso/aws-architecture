f1ff04519c71b984b24bd8ea823b7e40607157fd
Austin Corso <austinmcorso@gmail.com>
2016-07-31 13:19:47 -0700

    Adding EFS mount targets.

--- high_availability/terraform/efs.tf
+++ high_availability/terraform/efs.tf
@@ -4,3 +4,13 @@ resource "aws_efs_file_system" "default_efs" {
     Name = "${var.vpc_name}-efs"
   }
 }
+
+resource "aws_efs_mount_target" "default_us_east_1a_efs_mount_target" {
+  file_system_id = "${aws_efs_file_system.default_efs.id}"
+  subnet_id = "${aws_subnet.10010_us_east_1a_public.id}"
+}
+
+resource "aws_efs_mount_target" "default_us_east_1b_efs_mount_target" {
+  file_system_id = "${aws_efs_file_system.default_efs.id}"
+  subnet_id = "${aws_subnet.10020_us_east_1b_public.id}"
+}
