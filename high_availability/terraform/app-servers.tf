0749a3bfd9d1c057ca564498c9e00095f95954b4
Austin Corso <acorso@Austins-MBP.domain>
2016-07-31 11:20:24 -0700

    Split web servers between two public subnets across two az's.

--- high_availability/terraform/app-servers.tf
+++ high_availability/terraform/app-servers.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "web_one_public_server" {
     ami = "ami-6869aa05"
     count = "1"
-    subnet_id = "${aws_subnet.10020_us_east_1b_public.id}"
+    subnet_id = "${aws_subnet.10010_us_east_1a_public.id}"
     instance_type = "t2.micro"
     key_name = "${var.vpc_name}-keypair"
     security_groups = ["${aws_security_group.default_public_servers_sg.id}"]
