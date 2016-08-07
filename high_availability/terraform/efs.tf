bd3661cb724cb7fa2f84c785171a5b7eb2c9c19c
Chauncey Thorn <chaunceyt@gmail.com>
2016-08-06 22:45:07 -0400

    aws-architecture: adding efs mount points to private subnets, updating security groups for efs

--- high_availability/terraform/efs.tf
+++ high_availability/terraform/efs.tf
@@ -5,14 +5,126 @@ resource "aws_efs_file_system" "default_efs" {
   }
 }
 
-resource "aws_efs_mount_target" "default_efs_mount_target_one_efs_mount_target" {
-  file_system_id = "${aws_efs_file_system.default_efs.id}"
-  subnet_id = "${aws_subnet.default_subnet_one_public.id}"
-  security_groups = ["${aws_security_group.default_rds_sg.id}"]
+resource "aws_security_group" "default_security_group_target_one_public" {
+  name = "${var.vpc_name}_efs_one_public"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.default_vpc.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "TCP"
+    cidr_blocks = ["${var.public_subnet_one_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.vpc_name}_efs_one_public-sg"
+  }
 }
 
-resource "aws_efs_mount_target" "default_efs_mount_target_two_efs_mount_target" {
+resource "aws_security_group" "default_security_group_target_two_public" {
+  name = "${var.vpc_name}_efs_two_public"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.default_vpc.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "TCP"
+    cidr_blocks = ["${var.public_subnet_two_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.vpc_name}_efs_two_public-sg"
+  }
+}
+
+resource "aws_security_group" "default_security_group_target_one_private" {
+  name = "${var.vpc_name}_efs_one_private"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.default_vpc.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "TCP"
+    cidr_blocks = ["${var.private_subnet_one_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.vpc_name}_efs_one_private-sg"
+  }
+}
+
+resource "aws_security_group" "default_security_group_target_two_private" {
+  name = "${var.vpc_name}_efs_two_private"
+  description = "Allow all inbound traffic"
+  vpc_id = "${aws_vpc.default_vpc.id}"
+
+  ingress {
+    from_port = 0
+    to_port = 65535
+    protocol = "TCP"
+    cidr_blocks = ["${var.private_subnet_two_cidr}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.vpc_name}_efs_two_private-sg"
+  }
+}
+
+/* EFS mount points on public subnets */
+resource "aws_efs_mount_target" "default_efs_mount_target_one_public" {
+  file_system_id = "${aws_efs_file_system.default_efs.id}"
+  subnet_id = "${aws_subnet.default_subnet_one_public.id}"
+  security_groups = ["${aws_security_group.default_security_group_target_one_public.id}"]
+}
+
+resource "aws_efs_mount_target" "default_efs_mount_target_two_public" {
   file_system_id = "${aws_efs_file_system.default_efs.id}"
   subnet_id = "${aws_subnet.default_subnet_two_public.id}"
-  security_groups = ["${aws_security_group.default_rds_sg.id}"]
+  security_groups = ["${aws_security_group.default_security_group_target_two_public.id}"]
 }
+
+/* EFS mount points on private subnets */
+resource "aws_efs_mount_target" "default_efs_mount_target_one_private" {
+  file_system_id = "${aws_efs_file_system.default_efs.id}"
+  subnet_id = "${aws_subnet.default_subnet_one_private.id}"
+  security_groups = ["${aws_security_group.default_security_group_target_one_private.id}"]
+}
+
+resource "aws_efs_mount_target" "default_efs_mount_target_two_private" {
+  file_system_id = "${aws_efs_file_system.default_efs.id}"
+  subnet_id = "${aws_subnet.default_subnet_two_private.id}"
+  security_groups = ["${aws_security_group.default_security_group_target_two_private.id}"]
+}
+
+
