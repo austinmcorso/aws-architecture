a86ee3dd01f2fc96ef3f5d0bb375fb9a021d034f
Chauncey Thorn <chaunceyt@gmail.com>
2016-08-07 12:00:05 -0400

    aws-architecture: create s3 bucket for elb logging - need to resolve perm issue

--- high_availability/terraform/s3.tf
+++ high_availability/terraform/s3.tf
@@ -8,3 +8,15 @@ resource "aws_s3_bucket" "default_s3_bucket" {
         Environment = "prod"
     }
 }
+
+/* S3 bucket for elb logs */
+resource "aws_s3_bucket" "default_s3_log_bucket" {
+    bucket = "${var.vpc_name}_log_bucket"
+    acl = "log-delivery-write"
+
+    tags {
+        Name = "${var.vpc_name}_log_bucket"
+        Environment = "prod"
+    }
+}
+
