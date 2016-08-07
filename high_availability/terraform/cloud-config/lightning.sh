ef05454439b0f7b20fa23e00667f93e943bf12e5
Chauncey Thorn <chaunceyt@gmail.com>
2016-08-06 23:16:13 -0400

    aws-architecture: ec2 user_data scripts

---
+++ high_availability/terraform/cloud-config/lightning.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ANSIBLE_PULL=/opt/ansible/bin/ansible-pull
+ANSIBLE_PULL_REPO=https://github.com/chaunceyt/ansible-playbooks.git
+ANSIBLE_PULL_BRANCH=lightning
+
+# Install Git.
+yum -y install git
+
+# Checkout Ansible project code.
+cd /opt
+git clone git://github.com/ansible/ansible.git --recursive
+
+# Setup environment.
+source /opt/ansible/hacking/env-setup
+echo "localhost" > ~/ansible_hosts
+export ANSIBLE_HOSTS=~/ansible_hosts
+
+# Provision this host.
+${ANSIBLE_PULL}  -C $ANSIBLE_PULL_BRANCH -U $ANSIBLE_PULL_REPO
