86085058d8e536470180672eaf4b2fb7520e1730
Chauncey Thorn <chaunceyt@gmail.com>
2016-08-06 22:46:55 -0400

    aws-architecture: fixed bug

--- high_availability/terraform/elb.tf
+++ high_availability/terraform/elb.tf
@@ -10,7 +10,7 @@ resource "aws_elb" "default_elb" {
   ]
 
   listener {
-    instance_port = 8000
+    instance_port = 80
     instance_protocol = "http"
     lb_port = 80
     lb_protocol = "http"
@@ -20,7 +20,7 @@ resource "aws_elb" "default_elb" {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
-    target = "HTTP:8000/"
+    target = "HTTP:80/"
     interval = 30
   }
 
